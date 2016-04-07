cabf643a187ed6f69bb52bfc40594ce895406775
Keyur Patel <patelk36@mcmaster.ca>
2016-04-07 04:54:07 -0400

    final expert controls

--- Server/ExpertControl1.php
+++ Server/ExpertControl1.php
@@ -1,47 +1,49 @@
 <?php
-
+error_reporting(E_ALL) ;
+ini_set('display_errors','On');
 	class ExpertControl1 {
-		var $servername ;
-		var $username ;
-		var $password ;
-		var $dbname ;
-		var $conn ;
-
-		function _construct () {
+		public $servername ;
+		public $username ;
+		public $password ;
+		public $dbname ;
+		public $conn ;
+		
+		public function ExpertControl1() {
 			$this->servername = "localhost" ;
 			$this->username = "root" ;
 			$this->password = "bitnami" ;
 			$this->dbname = "experts" ;
 			$this->conn = new mysqli($this->servername,$this->username,$this->password,$this->dbname) ;
 
-			if (($this->conn)->connect_error) {
-				die("Connection Failed: " . ($this->conn)->connect_error) ;
+			if ($this->conn->connect_error) {
+				die("Connection Failed: " . $this->conn->connect_error) ;
 			}
 		}
 
 
-		function getResults($nameString) {
+		public function getResult($nameString) {
 			$names = explode(",",$nameString) ;
-			$sql = bindInValues($names) ;
-			$res = ($this->conn)->query($sql) ;
-			$this->conn->close() ;
-
-			if ($row = $res->fetch_row()) {	return $row[0] ; }
-		}
-
-		function bindInValues(array $values) {
-			$sql = sprintf('SELECT DISTINCT m.movie_name,
-			(SELECT count(*) as cnt from character_members m2 WHERE m2.movie_name=m.movie_name AND character_members IN (%s)) as order_col
+			//$res = ($conn)->query($sql) ;
+			$conn = new mysqli($this->servername,$this->username,$this->password,$this->dbname) ;
+			
+			$sql = sprintf("SELECT DISTINCT m.movie_name,
+			(SELECT count(*) as cnt from character_members m2 WHERE m2.movie_name=m.movie_name AND character_members IN ('%s')) as order_col
 			FROM character_members m
 			ORDER BY order_col DESC
-			LIMIT 1;',implode(', ', array_fill(0, count($values), '?')));
+			LIMIT 1;",implode("', '", $names));
 
-			$stmt = ($this->conn)->prepare($sql);
-
-			foreach ($values as $value) {
-				$stmt->bind_param('s', $value);
+			$stmt = $conn->prepare($sql);
+			
+			$stmt->execute() ;
+			$stmt->bind_result($movieName,$weight);
+			
+			if ($stmt->fetch()) {
+				if ($weight>0) {return $movieName ; }
+				else { return 'No Movie Found' ;}
 			}
-			return $stmt;
+			
+			$stmt->close() ;
+			$conn->close() ;
 		}
 
 	}
