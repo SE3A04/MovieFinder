b9cbff9b8356a1ccc2e4fe1d3a45642162a5f149
Justin Kapinski <justinkapinski@hotmail.com>
2016-04-07 00:22:00 -0400

    Filled in most of google maps activity

--- Android/MovieMatcher/app/src/main/java/bestcompany/moviematcher/MapsActivity.java
+++ Android/MovieMatcher/app/src/main/java/bestcompany/moviematcher/MapsActivity.java
@@ -1,5 +1,6 @@
 package bestcompany.moviematcher;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,9 +11,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String SERVER_ADDRESS = "13.82.49.1";
+
+    private Protocol protocol = null;
     private GoogleMap mMap;
+    private String currentMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        currentMovie = getIntent().getStringExtra("SelectedMovie");
        // setUpMapIfNeeded(); now
     }
 
@@ -63,9 +72,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        new ConnectTask().execute();
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private class ConnectTask extends AsyncTask<Void, Void, Protocol> {
+
+        @Override
+        protected Protocol doInBackground(Void... params) {
+            Protocol p = null;
+            try {
+                InputStream caCert = getAssets().open("ca.crt");
+                p = new Protocol(caCert, SERVER_ADDRESS);
+            } catch (Exception e) {}
+
+            return p;
+        }
+
+        @Override
+        protected void onPostExecute(Protocol p) {
+            if (p != null) {
+                protocol = p;
+                new GetLocationsTask().execute();
+            } else {
+                // failed to create connection
+            }
+        }
+    }
+
+    private class GetLocationsTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String data = "";
+            Map<String, String> map = new HashMap<>();
+            map.put("Name", params[0]);
+
+            try {
+                data = protocol.securePostRequest("MovieMatcherServer/location.php", true, map);
+            } catch (Exception e) {}
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            if (data.length() > 0) {
+                String[] locations = data.split("@");
+
+                // add markers here
+
+            } else {
+                // failed to get locations
+            }
+        }
     }
 }
